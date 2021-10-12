c4d64d0d948eea725fd4ef8c658b9101a7c4e5e8
Adam <adam.hewett@veolia.com>
2021-10-12 08:23:59 +0100

    added test-bot stack

--- bin/telegram-bots.ts
+++ bin/telegram-bots.ts
@@ -3,6 +3,9 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 const app = new cdk.App();
 
+import { TestBotStack } from '../bots/test-bot/test-bot-stack';
+new TestBotStack(app, 'TestBotStack', {});
+
 import { Only1TokenBotStack } from '../bots/o1t-bot/o1t-bot-stack';
 new Only1TokenBotStack(app, 'Only1TokenBotStack', {});
 
---
+++ bots/test-bot/test-bot-stack.ts
@@ -0,0 +1,53 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as logs from '@aws-cdk/aws-logs';
+
+export class TestBotStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const botName = 'test-bot';
+        const path = `bots/${botName}/lambda`;
+        const layerPath = 'shared/lambda-layers';
+
+        const botLibsLayer = new lambda.LayerVersion(this, 'bot-libs', {
+            code: lambda.Code.fromAsset(`${layerPath}/bot-libs`),
+            layerVersionName: 'bot-libs-layer',
+            compatibleRuntimes: [lambda.Runtime.PYTHON_3_8],
+        });
+
+        const lbd = new lambda.Function(this, `${botName}-lambda`, {
+            runtime: lambda.Runtime.PYTHON_3_8,
+            handler: `${botName}.lambda_handler`,
+            code: lambda.Code.fromAsset(`${path}/${botName}`),
+            environment: {
+                telegramApiKey: '',
+                BOT_NAME: botName,
+            },
+            layers: [botLibsLayer],
+            logRetention: logs.RetentionDays.ONE_WEEK,
+            timeout: cdk.Duration.seconds(10),
+        });
+
+        const restAPI = new apigateway.RestApi(this, `${botName}-api`, {
+            restApiName: botName,
+            deploy: true,
+            deployOptions: {
+                loggingLevel: apigateway.MethodLoggingLevel.ERROR,
+            },
+        });
+
+        const apiUsagePlan = restAPI.addUsagePlan(`${botName}-usage-plan`, {
+            name: `${botName}-usage-plan`,
+        });
+
+        apiUsagePlan.addApiStage({ stage: restAPI.deploymentStage });
+
+        const botResource = restAPI.root.addResource('WpnCqakM6XBcNejEsAseDTWjhSJQgbJ2Cb6jV45ZHKCk3GRQcsnGGPdBPYM3');
+
+        botResource.addMethod('POST', new apigateway.LambdaIntegration(lbd), {
+            apiKeyRequired: false,
+        });
+    }
+}
